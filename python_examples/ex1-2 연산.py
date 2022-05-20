f8b96500539b5f309c37d17aba86197ee445a4f4
park sang hoon <64889584+qwxe@users.noreply.github.com>
2022-05-20 15:07:53 +0900

    Delete python_examples directory

--- python_examples/ex1-2 연산.py
+++
@@ -1,8 +0,0 @@
-a = 10
-b = 7
-
-print('a+b = ', a+b) # 더하기 연산
-print('a-b = ', a-b) # 빼기 연산
-print('a*b = ', a*b) # 곱셈 연산
-print('a/b = ', a/b) # 나눗셈 연산
-print('a%b = ', a%b) # 나머지 연산 (a를 b로 나누었을 때 나머지)
--- python_examples/ex7-clnt 소켓.py
+++
@@ -1,7 +0,0 @@
-from socket import *
-
-clntskt = socket(AF_INET, SOCK_STREAM)  # TCP , IPv4로 클라이언트 소켓 생성 
-clntskt.connect(('127.0.0.1', 8080))    # 루프백 IP, 포트 번호 8080로 소켓 할당
-
-message = input()                       # 메시지를 입력받음
-clntskt.send(message.encode('utf-8'))   # 메시지를 서버에 전송
